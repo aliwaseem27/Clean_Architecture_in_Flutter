dc9e1a184d7edcddd188433f9407479516441f65
ali.waseem <ali.waseem@arabmarketcap.com>
2023-04-06 04:43:26 +0300

    fix: small change to test ssh fix

--- clean_a/lib/src/core/utils/app_strings.dart
+++ clean_a/lib/src/core/utils/app_strings.dart
@@ -3,4 +3,5 @@ class AppStrings{
   static const String fontFamily = "";
   static const String noRouteFound = "No Route Found";
   static const String title = "Quotes";
+  static const String refresh = "Refresh";
 }
