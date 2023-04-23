7c63112917f42c4140c521fb347520ee432606a2
ali.waseem <ali.waseem27@gmail.com>
2023-04-23 13:57:17 +0300

    add: quote model

---
+++ clean_a/lib/src/features/random_quote/data/models/quote_model.dart
@@ -0,0 +1,55 @@
+import 'package:clean_a/src/features/random_quote/domain/entities/quote.dart';
+
+class QuoteModel extends Quote {
+   QuoteModel({
+    required this.id,
+    required this.content,
+    required this.author,
+    required this.tags,
+    required this.authorSlug,
+    required this.length,
+    required this.dateAdded,
+    required this.dateModified,
+  }) : super(
+            id: id,
+            content: content,
+            author: author,
+            tags: tags,
+            authorSlug: authorSlug,
+            length: length,
+            dateAdded: dateAdded,
+            dateModified: dateModified);
+
+  String id;
+  String content;
+  String author;
+  List<String> tags;
+  String authorSlug;
+  int length;
+  DateTime dateAdded;
+  DateTime dateModified;
+
+  factory QuoteModel.fromJson(Map<String, dynamic> json) => QuoteModel(
+        id: json["_id"],
+        content: json["content"],
+        author: json["author"],
+        tags: List<String>.from(json["tags"].map((x) => x)),
+        authorSlug: json["authorSlug"],
+        length: json["length"],
+        dateAdded: DateTime.parse(json["dateAdded"]),
+        dateModified: DateTime.parse(json["dateModified"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "_id": id,
+        "content": content,
+        "author": author,
+        "tags": List<dynamic>.from(tags.map((x) => x)),
+        "authorSlug": authorSlug,
+        "length": length,
+        "dateAdded":
+            "${dateAdded.year.toString().padLeft(4, '0')}-${dateAdded.month.toString().padLeft(2, '0')}-${dateAdded.day.toString().padLeft(2, '0')}",
+        "dateModified":
+            "${dateModified.year.toString().padLeft(4, '0')}-${dateModified.month.toString().padLeft(2, '0')}-${dateModified.day.toString().padLeft(2, '0')}",
+      };
+}
--- clean_a/lib/src/features/random_quote/domain/entities/quote.dart
+++ clean_a/lib/src/features/random_quote/domain/entities/quote.dart
@@ -2,13 +2,23 @@ import 'package:equatable/equatable.dart';
 
 class Quote extends Equatable {
   final String id;
-  final String author;
   final String content;
+  final String author;
+  final List<String> tags;
+  final String authorSlug;
+  final int length;
+  final DateTime dateAdded;
+  final DateTime dateModified;
 
   const Quote({
     required this.id,
-    required this.author,
     required this.content,
+    required this.author,
+    required this.tags,
+    required this.authorSlug,
+    required this.length,
+    required this.dateAdded,
+    required this.dateModified,
   });
 
   @override
