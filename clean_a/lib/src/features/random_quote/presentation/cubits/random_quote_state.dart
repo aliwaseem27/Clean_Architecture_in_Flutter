1eff12b7a0278a8e5dec1c2dd4f10fc75cbfc5bf
ali.waseem <ali.waseem27@gmail.com>
2023-05-01 23:21:17 +0300

    add: random quote cubit and state

---
+++ clean_a/lib/src/features/random_quote/presentation/cubits/random_quote_state.dart
@@ -0,0 +1,10 @@
+part of 'random_quote_cubit.dart';
+
+abstract class RandomQuoteState extends Equatable {
+  const RandomQuoteState();
+}
+
+class RandomQuoteInitial extends RandomQuoteState {
+  @override
+  List<Object> get props => [];
+}
