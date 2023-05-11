760ede82757b5cef33fbd844c8b5ef477ddc4502
ali.waseem <ali.waseem27@gmail.com>
2023-05-11 19:32:17 +0300

    :

---
+++ clean_a/lib/src/injection_container.dart
@@ -0,0 +1,47 @@
+import 'package:clean_a/src/core/network/network_info.dart';
+import 'package:clean_a/src/features/random_quote/data/datasources/random_quote_local_data_source.dart';
+import 'package:clean_a/src/features/random_quote/data/datasources/random_quote_remote_data_source.dart';
+import 'package:clean_a/src/features/random_quote/data/repositories/quote_repository_impl.dart';
+import 'package:clean_a/src/features/random_quote/domain/repositories/quote_repository.dart';
+import 'package:clean_a/src/features/random_quote/domain/usecases/get_random_quote.dart';
+import 'package:clean_a/src/features/random_quote/presentation/cubits/random_quote_cubit.dart';
+import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  // Features
+  // Blocs
+  sl.registerFactory(() => RandomQuoteCubit(getRandomQuoteUseCase: sl()));
+
+  // Usecases
+  sl.registerLazySingleton(() => GetRandomQuote(quoteRepository: sl()));
+
+  // Repositories
+  sl.registerLazySingleton<QuoteRepository>(
+    () => QuoteRepositoryImpl(
+        networkInfo: sl(),
+        randomQuoteRemoteDataSource: sl(),
+        randomQuoteLocalDataSource: sl()),
+  );
+
+  //Datasources
+  sl.registerLazySingleton<RandomQuoteRemoteDataSource>(
+      () => RandomQuoteRemoteDataSourceImpl(client: sl()));
+
+  sl.registerLazySingleton<RandomQuoteLocalDataSource>(
+      () => RandomQuoteLocalDataSourceImpl(sharedPreferences: sl()));
+
+  //! Core
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(connectionChecker: sl()));
+
+  //! External
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => sharedPreferences);
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => InternetConnectionChecker());
+
+}
