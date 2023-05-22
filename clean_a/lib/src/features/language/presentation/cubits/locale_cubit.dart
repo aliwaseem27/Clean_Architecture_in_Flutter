83e8b9800a0a729810d2444f994b6f4df173b45f
ali.waseem <ali.waseem27@gmail.com>
2023-05-22 18:08:07 +0300

    update: locale cubit

--- clean_a/lib/src/features/language/presentation/cubits/locale_cubit.dart
+++ clean_a/lib/src/features/language/presentation/cubits/locale_cubit.dart
@@ -1,14 +1,41 @@
+import 'package:clean_a/src/core/usecases/usecase.dart';
 import 'package:clean_a/src/core/utils/app_strings.dart';
+import 'package:clean_a/src/features/language/domain/usecases/change_locale.dart';
+import 'package:clean_a/src/features/language/domain/usecases/get_saved_lang.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'locale_state.dart';
 
 class LocaleCubit extends Cubit<LocaleState> {
-  LocaleCubit() : super(ChangeLocaleState(Locale(AppStrings.englishCode)));
+  final GetSavedLangUseCase getSavedLangUseCase;
+  final ChangeLangUseCase changeLangUseCase;
 
-// get saved language from shared prefrences
+  LocaleCubit(
+      {required this.getSavedLangUseCase, required this.changeLangUseCase})
+      : super(ChangeLocaleState(Locale(AppStrings.englishCode)));
 
-// change Locale
+  String currentLangCode = AppStrings.englishCode;
+
+  Future<void> getSavedLang() async {
+    final response = await getSavedLangUseCase.call(NoParams());
+    response.fold((failure) => debugPrint(AppStrings.cacheFailure), (value) {
+      currentLangCode = value;
+      emit(ChangeLocaleState(Locale(currentLangCode)));
+    });
+  }
+
+  Future<void> _changeLang(String langCode) async {
+    final response = await changeLangUseCase.call(langCode);
+    response.fold((failure) => debugPrint(AppStrings.cacheFailure), (value) {
+      currentLangCode = langCode;
+      emit(ChangeLocaleState(Locale(currentLangCode)));
+    });
+  }
+
+  void toEnglish() => _changeLang(AppStrings.englishCode);
+
+  void toArabic() => _changeLang(AppStrings.arabicCode);
 }
